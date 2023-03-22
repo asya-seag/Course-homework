feba5a4f747f3e3a0be374265a173b4192567d0d
asya-seag <asyaseagrave@gmail.com>
2023-03-22 18:36:46 +0000

    Initial commit

---
+++ cat_food.py
@@ -0,0 +1,12 @@
+cats = 10
+cans_per_day = 2
+
+total_cans = cats * cans_per_day
+
+output = str(cats) + " cats eat " + str(total_cans) + " cans"
+
+print(output)
+
+
+total = cats * cans_per_day
+print("{} cats eat {} cans per day.".format(cats, total))
