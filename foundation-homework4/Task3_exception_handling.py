2437a2301fb56c7525dc16218d19320ed4accf3d
asya-seag <asyaseagrave@gmail.com>
2023-03-28 22:50:26 +0100

    finished assignment

--- foundation-homework4/Task3_exception_handling.py
+++ foundation-homework4/Task3_exception_handling.py
@@ -9,9 +9,10 @@ store_dict = {
 #entering money available for customer
 money_available = 100
 
+
 #Welcome the customer and display the items and their prices, along with an option to “exit”
 print(f"Welcome to our shop, please view the items \n {store_dict}. ")
-exit = input("Would you like to exit our online store?" )
+exit = input("Would you like to exit our online store? (yes/no) " )
 if exit == "yes":
     print("Thank you for visiting our online store!")
 elif exit == "no":
@@ -20,19 +21,46 @@ else:
      print("Wrong answer, answer yes or no")
 # Accept the option as an input, an invalid input should raise a ValueError
 
-total_price=0
+
+
 try:
         if customer_selection not in store_dict:
             raise ValueError
         else:
             for keys, values in store_dict.items():
                 if keys == customer_selection:
-                    total_price = total_price + store_dict[keys]
-                    print(total_price)
-
+                    final_price = 0 + store_dict[keys]
 
 except ValueError:
             print("ValueError, there is no such item in the shop")
 
-# If the customer can afford it, print out a message saying “Here’s your {item}!”
+
+
+counter = 0
+if final_price <= money_available:
+    print(f"Here’s your {customer_selection}!")
+    print("Thank you for visiting our online shop")
+else:
+    for counter in range(3):
+        do_you_have_more = int(input("Insufficient funds. Would you like to add more money? Enter a number here:"))
+        if do_you_have_more + money_available >= final_price:
+            print(f"Here’s your {customer_selection}!")
+            print("Thank you for visiting our online shop")
+            break
+
+
+    else:
+        print("Error can not exceed three attempts")
+
+
+
+
+
+
+
+
+
+
+
+
 
